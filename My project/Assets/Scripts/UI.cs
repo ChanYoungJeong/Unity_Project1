0fefe138717d7fb0d5e07f35f0813c359c46306d
byungwoo0601 <rlaquddn0601@naver.com>
2022-07-06 08:13:43 +0900

    체력바 감소
    +딜레이작업 필요

--- My project/Assets/Scripts/UI.cs
+++ My project/Assets/Scripts/UI.cs
@@ -5,8 +5,31 @@ public class UI : MonoBehaviour
 {
     public Slider HP_bar;
 
+<<<<<<< Updated upstream
     public float maxHp = 100;        // 몬스터 총 HP
     public float curHp = 100;         // 몬스터 현제 HP
+=======
+    private float maxHp = 100;
+    private float curHp = 100;
+
+    public GameObject healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP_bar.value = (float)curHp / (float)maxHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            curHp -= 10;
+        }
+
+    }
+>>>>>>> Stashed changes
 
     private void Awake()
     {
