f169b1d9a5f755534673480fa2a77866b5ee5086
ChanYoungJeong <43865103+ChanYoungJeong@users.noreply.github.com>
2022-07-22 02:26:47 -0700

    Update : Skill System

--- My project/Assets/Scripts/Combat_Manager.cs
+++ My project/Assets/Scripts/Combat_Manager.cs
@@ -6,11 +6,13 @@ public class Combat_Manager : MonoBehaviour
 {
     PlayerScript Player_Status;
     Monster_Script Monster_Status;
+    Skill_Manager Skills;
     int Combat_Counter = 1;
     // Start is called before the first frame update
     void Start()
     {
         Player_Status = transform.GetComponentInParent<PlayerScript>();
+        Skills = transform.parent.GetComponentInChildren<Skill_Manager>();
     }
 
     // Update is called once per frame
@@ -28,16 +30,34 @@ public class Combat_Manager : MonoBehaviour
 
     private void Active_Attack(GameObject Monster)
     {
-        StartCoroutine(Basic_Attack(Monster));
+        if (Skills.Skill_Lists.Count != 0)
+        {
+            for (int i = 0; i < Skills.Skill_Lists.Count; i++)
+            {
+                Skills.StartCoroutine(Skills.Start_Timer(Skills.Skill_Lists[0]));
+                Skills.StartCoroutine(Skills.Active_Skill(Skills.Skill_Lists[0], Monster));
+            }
+            Combat_Counter = 1;
+        }
+        else
+        {
+            StartCoroutine(Basic_Attack(Monster));
+        }
     }
 
     IEnumerator Basic_Attack(GameObject Monster)
     {
         Monster_Status = Monster.GetComponent<Monster_Script>();
         Monster_Status.nowHp -= Player_Status.atkDmg;
+        Debug.Log(Monster_Status.nowHp);
         yield return new WaitForSeconds(Player_Status.atkSpeed);
         Combat_Counter = 1;
-        Debug.Log("Active?");
+        
+    }
+
+    IEnumerator Attack_Duration()
+    {
+        yield return new WaitForSeconds(Player_Status.atkSpeed);
     }
 
 }
---
+++ My project/Assets/Skill_Manager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_Manager : MonoBehaviour
+{
+    PlayerScript Player_Status;
+    public List<Skills> Skill_Lists = new List<Skills>();
+    private void Awake()
+    {
+        Player_Status = transform.GetComponentInParent<PlayerScript>();
+        Generate();
+    }
+    
+    //Generate Skills
+    void Generate()
+    {
+        Skills skill_0 = new Skills(0, "Double Slash", 0.7f, 0.25f, 3.0f, 2, 1);
+        Skills skill_1 = new Skills(0, "Fire Slash", 0.5f, 0.1f, 5.0f, 7, 1);
+
+
+        //Add Skills to the list
+        Skill_Lists.Add(skill_0);
+        Skill_Lists.Add(skill_1);
+    }
+
+    public IEnumerator Active_Skill(Skills skill_, GameObject Monster)
+    {
+        Monster_Script Monster_Status = Monster.GetComponent<Monster_Script>();
+        Debug.Log("Active " + skill_.Name);
+        Skill_Lists.Remove(skill_);
+        for (int i = 0; i < skill_.Number_of_Attack; i++)
+        {             
+            Monster_Status.nowHp -= (int)(Player_Status.atkDmg * skill_.Damage);
+            Debug.Log("Apply " + skill_.Name + " Damage "+ (int)(Player_Status.atkDmg * skill_.Damage) + " Monster Hp :" + Monster_Status.nowHp);
+            yield return new WaitForSeconds(skill_.Duration);
+        }
+        //StartCoroutine(Start_Timer(skill_));
+    }
+
+    public IEnumerator Start_Timer(Skills skill_)
+    {
+        Debug.Log(skill_.Name + "Cool Down Starts");
+        yield return new WaitForSeconds(skill_.Cooldown);
+        if(!Skill_Lists.Find(x => x.id == skill_.id))
+        {
+            Skill_Lists.Add(skill_);
+        }
+        Debug.Log(skill_.Name + "Cool Down Done");
+    }
+
+}
