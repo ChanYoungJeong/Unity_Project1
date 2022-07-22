f0cb81c03f7384bcb20436ec675d0cef0140022a
ChanYoungJeong <43865103+ChanYoungJeong@users.noreply.github.com>
2022-07-21 19:02:00 -0700

    Update : Monster Script Name Change

--- My project/Assets/Scripts/Monster_Script.cs
+++ My project/Assets/Scripts/Monster_Script.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Monster : MonoBehaviour
+public class Monster_Script : MonoBehaviour
 {
+    // Start is called before the first frame update
     [Header("monster stats")]
     public string monsterName;
     public int maxHp;
@@ -28,24 +29,24 @@ public class Monster : MonoBehaviour
     {
         maxHp = 100;
         nowHp = 100;
-        
-        hpBar = Instantiate(prfHpbar, canvas.transform).GetComponent<RectTransform>();             
+
+        hpBar = Instantiate(prfHpbar, canvas.transform).GetComponent<RectTransform>();
         if (name.Equals("Monster1(Clone)"))
         {
             SetMonsterStatus("Monster1(Clone)", 100, 10, 1);
         }
         nowHpbar = hpBar.transform.GetChild(0).GetComponent<Image>();
-        
+
     }
     private
     void Update()
     {
-        
-        Vector3 _hpBarPos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y+height, 0));  
-        hpBar.position = _hpBarPos;                                                                                           
+
+        Vector3 _hpBarPos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + height, 0));
+        hpBar.position = _hpBarPos;
         nowHpbar.fillAmount = (float)nowHp / (float)maxHp;
     }
-    
+
     private void SetMonsterStatus(string _Monster_Name, int _maxHp, int _atkDmg, int _atkSpeed)
     {
         monsterName = _Monster_Name;
@@ -54,6 +55,5 @@ public class Monster : MonoBehaviour
         atkDmg = _atkDmg;
         atkSpeed = _atkSpeed;
     }
-
 
 }
