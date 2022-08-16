c0be39fe2e5191dac06334126be9d433fcadc8d9
hongjeonghoon <108395888+hongjeonghoon@users.noreply.github.com>
2022-08-16 13:19:18 +0900

    update

---
+++ My project/Assets/Scripts/Interface/Card.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CardGrade {sss,aa,b,f}
+[System.Serializable]
+public class Card
+{
+    public string cardName;
+    public Sprite cardImage;
+    public CardGrade cardGrade;
+    public int weight;
+
+    public Card(Card card)
+    {
+        this.cardName = card.cardName;
+        this.cardImage = card.cardImage;
+        this.cardGrade = card.cardGrade;
+        this.weight = card.weight;
+    }
+}
