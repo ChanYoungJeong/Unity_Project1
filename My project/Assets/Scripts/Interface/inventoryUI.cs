b6d8c3e0643e4a664174af0cda1c5190ab367dfb
ChanYoungJeong <43865103+ChanYoungJeong@users.noreply.github.com>
2022-09-10 19:36:04 -0700

    Update : on_Battle update, Inventory logic modified

--- My project/Assets/Scripts/Interface/inventoryUI.cs
+++ My project/Assets/Scripts/Interface/inventoryUI.cs
@@ -7,18 +7,12 @@ public class inventoryUI : MonoBehaviour
 {
     public GameObject inventoryPanel;
     bool activeInventory = false;
-    public Slot[] slots;
-    int numSlots;
-    public Transform slotHolder;
 
     private void Awake()
     {
-        slots = slotHolder.GetComponentsInChildren<Slot>();
         inventoryPanel.SetActive(activeInventory);
-        numSlots = slots.Length;
-        //Debug.Log(numSlots);
     }
-    
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.I))
@@ -28,23 +22,5 @@ public class inventoryUI : MonoBehaviour
         }
     }
 
-    public void SetItem(string itemName)
-    {
-        for (int i = 0;i < numSlots; i++)
-        {
-            if(!slots[i].hasItem)
-            {
-                slots[i].transform.GetChild(0).GetComponentInChildren<Image>().sprite = GetImage(itemName);
-                slots[i].hasItem = true;
-                break;
-            }
-        }
-    }
 
-    private Sprite GetImage(string itemName)
-    {
-        Sprite image = Resources.Load<Sprite>("Image/Equipment/" + itemName);
- 
-        return image;
-    }
 }
