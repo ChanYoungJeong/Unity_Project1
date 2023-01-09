8df02a7c9abf6fd57237cb2f5d5bdd5ad9457196
byungwoo0601 <rlaquddn0601@naver.com>
2023-01-10 00:52:44 +0900

    20230110
    .

--- My project/Assets/Scripts/Raid/BossMonster.cs
+++ My project/Assets/Scripts/Raid/BossMonster.cs
@@ -8,8 +8,4 @@ public class BossMonster : MonoBehaviour
     public string monsterType;
     public float maxHp;
     public float nowHp;
-    public float atkDmg;
-    public float atkSpeed;
-    public int Golds;
-    public int Exp;
 }
--- My project/Assets/Scripts/Raid/Boss_HpBar.cs
+++ My project/Assets/Scripts/Raid/Boss_HpBar.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Boss_HpBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Slider hpSlider;
+    public Canvas hpCanvas;
+    [SerializeField]
+    BossMonster BossStat;
+    float maxHp;
+    float curHp;
 
-    // Update is called once per frame
+    void Awake()
+    {
+        BossStat = this.GetComponent<BossMonster>();
+    }
     void Update()
     {
-        
+        maxHp = BossStat.maxHp;
+        curHp = BossStat.nowHp;
+        hpCanvas.transform.position = this.transform.position;
+        hpSlider.value = Mathf.Lerp(hpSlider.value, curHp / maxHp, Time.deltaTime * 5f);
     }
 }
