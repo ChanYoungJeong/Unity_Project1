24ef776d6ac4c19515b6912e11eb2db4b6225a6d
byungwoo0601 <rlaquddn0601@naver.com>
2023-02-02 22:39:45 +0900

    2023 02 02
    서브컨텐츠 버그 수정

--- My project/Assets/Scripts/SubContents/PoolManager.cs
+++ My project/Assets/Scripts/SubContents/PoolManager.cs
@@ -22,21 +22,8 @@ public class PoolManager : MonoBehaviour
     {
         GameObject select = null;
 
-        foreach(GameObject item in pools[index])
-        {
-            if (!item.activeSelf)
-            {
-                select = item;
-                select.SetActive(true);
-                break;
-            }
-        }
-
-        if (!select)
-        {
-            select = Instantiate(prefabs[index], transform);
-            pools[index].Add(select);
-        }
+        select = Instantiate(prefabs[index], transform);
+        pools[index].Add(select);
 
         return select;
     }
