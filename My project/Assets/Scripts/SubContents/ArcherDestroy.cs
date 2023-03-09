b002bcbc3362c6cbbdb72787eecd5511996ae669
hongjeonghoon <108395888+hongjeonghoon@users.noreply.github.com>
2023-03-09 17:18:20 +0900

    Revert "skill"
    This reverts commit d76126b2823a7f82864feff6896654a278555ec6.

--- My project/Assets/Scripts/SubContents/ArcherDestroy.cs
+++ My project/Assets/Scripts/SubContents/ArcherDestroy.cs
@@ -4,37 +4,19 @@ using UnityEngine;
 
 public class ArcherDestroy : MonoBehaviour
 {
-    public GameObject archerskill;
-
-    
     private void Start()
     {
         Archerskill();
-        Invoke("DestroyScript", 1.0f);
+        Invoke("DestroyScript", 2.0f);
     }
 
     void Archerskill()
     {
-        int speed = 20;
-        float angle = 0;
-        //ArcherAsder.transform.position=new Vector3(0,0,angle);
-        //위치값
-
-
-        /*angle = Mathf.Atan2(Battle_Situation_Trigger.monster.transform.position.y - this.transform.position.y,
-                Battle_Situation_Trigger.monster.transform.position.x - this.transform.position.x) * Mathf.Rad2Deg;*/
-
-
-        GameObject ArcherAsder = Instantiate(archerskill, transform.position, Quaternion.identity);
-
-        ArcherAsder.GetComponent<Rigidbody2D>().AddForce(ArcherAsder.transform.right * speed, ForceMode2D.Impulse);
-
-        Destroy(ArcherAsder, 2.0f); //각도 너가 말한코드
 
     }
 
     void DestroyScript()
-    { 
+    {
         Destroy(gameObject);
     }
 }
