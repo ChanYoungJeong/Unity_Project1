d0724e88c6ac353621108c39a4278d291ddf04ae
chan0213 <97275826+chan0213@users.noreply.github.com>
2023-04-04 22:41:33 +0900

    .

--- My project/Assets/Scripts/Interface/InterfaceSize.cs
+++ My project/Assets/Scripts/Interface/InterfaceSize.cs
@@ -6,7 +6,7 @@ public class InterfaceSize : MonoBehaviour
 { 
 
 
-    void Start()
+    void Awake()
     {
         SetResolution(); // 초기에 게임 해상도 고정
     }
@@ -14,11 +14,13 @@ public class InterfaceSize : MonoBehaviour
     /* 해상도 설정하는 함수 */
     public void SetResolution()
     {
-        int setWidth = 1440; // 사용자 설정 너비
+        int setWidth = 1920; // 사용자 설정 너비
         int setHeight = 1080; // 사용자 설정 높이
 
         int deviceWidth = Screen.width; // 기기 너비 저장
         int deviceHeight = Screen.height; // 기기 높이 저장
+
+        //Screen.SetResolution and Camera.main.rect 공부
 
         Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true); // SetResolution 함수 제대로 사용하기
 
@@ -30,7 +32,7 @@ public class InterfaceSize : MonoBehaviour
         else // 게임의 해상도 비가 더 큰 경우
         {
             float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight); // 새로운 높이
-            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용
+            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용     
         }
     }
    
