200054b6c2864624c7f4d5c752a545841e55d82e
hongjeonghoon <108395888+hongjeonghoon@users.noreply.github.com>
2023-04-19 10:27:28 +0900

    서브컨텐츠(버튼 이벤트) 플레이어 오류 수정

--- My project/Assets/Scripts/SubContents/AsderCall.cs
+++ My project/Assets/Scripts/SubContents/AsderCall.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using static UnityEditor.ShaderData;
 
 public class AsderCall : MonoBehaviour
 {
+    public Button AsderButton;
     GetAsderBead Getasderbead;
     public Transform PlayerTrans;
     private Joystick joystick;
     public GameObject SubAsder;
+
     public float y;
     public int BeadCount;
+    public float DestroyCount;
    
     
     private void Awake()
@@ -21,11 +25,25 @@ public class AsderCall : MonoBehaviour
             joystick = GameObject.FindObjectOfType<Joystick>();
         }
     }
-  
+    private void Update()
+    {
+        OnOffAsderButton();
+    }
+
+    public void OnOffAsderButton()
+    {
+        if (BeadCount > 0)
+        {
+            AsderButton.interactable = true;
+        }
+        else
+            AsderButton.interactable = false;
+    }
     public void CallAsder()
     {
         if (BeadCount > 0)
         {
+
             /*if (joystick.Horizontal != 0 || joystick.Vertical != 0)
             {
                 if (joystick.Horizontal > 0)
@@ -38,10 +56,12 @@ public class AsderCall : MonoBehaviour
                 }
             }*/
 
-                GameObject Asder = Instantiate(SubAsder, PlayerTrans.position + Vector3.right * 1.0f, Quaternion.identity);
-                Asder.transform.rotation = Quaternion.Euler(0, y, 0);
-            
-            BeadCount--;
+            GameObject Asder = Instantiate(SubAsder, PlayerTrans.position + Vector3.right * 1.0f, Quaternion.identity);
+            Asder.transform.rotation = Quaternion.Euler(0, y, 0);
+
+            Destroy(Asder,DestroyCount);
         }
+       
+        BeadCount--;
     }
 }
