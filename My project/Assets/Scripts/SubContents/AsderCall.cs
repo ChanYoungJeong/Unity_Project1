6b41cff6c20b52852acd1df3311daebb59bac65c
ChanYoungJeong <43865103+ChanYoungJeong@users.noreply.github.com>
2023-04-27 10:26:19 -0700

    코멘트 추가

--- My project/Assets/Scripts/SubContents/AsderCall.cs
+++ My project/Assets/Scripts/SubContents/AsderCall.cs
@@ -38,6 +38,7 @@ public class AsderCall : MonoBehaviour
         thisButton = GetComponent<Button>();
 
         //임시용 for문
+        //heroSummon의 리스트에서 현재 버튼에 할당된 Type명에 따라 소환할 prefab과 이미지를 불러옴
         for (int i = 0; i < heroSummon.HeroList.Length; i++)
         {
             if (Type == heroSummon.HeroList[i].name)
@@ -53,6 +54,7 @@ public class AsderCall : MonoBehaviour
 
     private void Update()
     {
+        //이 AsderCall의 beadCount에 따라 버튼 누르는 여부 설정
         if (beadCount > 0)
             thisButton.interactable = true;
         else
