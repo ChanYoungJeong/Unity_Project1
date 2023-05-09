4046da8b5cf0e642bd9de6dcf78750ded34f26fb
byungwoo0601 <rlaquddn0601@naver.com>
2023-05-09 18:58:41 +0900

    20230509
    단일타겟복수공격 구현

--- My project/Assets/Scripts/Sub Character/Re_subChar/MaintainSubCharacterData.cs
+++ My project/Assets/Scripts/Sub Character/Re_subChar/MaintainSubCharacterData.cs
@@ -7,7 +7,14 @@ public class MaintainSubCharacterData : MonoBehaviour
     public Stat[] Char;
 
 
+
+    [Header("다중공격 옵션")]
     public bool[] multishot;
+    public int[] numberOfProjectiles;
+
+    [Header("단일공격 옵션")]
+    public bool[] singleshot;
+    public int[] numberOfSingleProjectiles;
 
     [Header("종족")]
     public string[] type;
@@ -33,6 +40,9 @@ public class MaintainSubCharacterData : MonoBehaviour
     private void Awake()
     {
         multishot = new bool[Char.Length];
+        numberOfProjectiles = new int[Char.Length];
+        singleshot = new bool[Char.Length];
+        numberOfSingleProjectiles = new int[Char.Length];
         type = new string[Char.Length];
         id = new int[Char.Length];
         maxHp = new float[Char.Length];
@@ -62,7 +72,7 @@ public class MaintainSubCharacterData : MonoBehaviour
     {
         time += Time.deltaTime;
 
-        if(maxtime <= time)
+        if (maxtime <= time)
         {
             time = 0;
             UpdateSubCharaterStatData();
@@ -71,21 +81,30 @@ public class MaintainSubCharacterData : MonoBehaviour
 
     void UpdateSubCharaterStatData()
     {
-        for(int i = 0; i<Char.Length; i++)
+        for (int i = 0; i < Char.Length; i++)
         {
-            for(int j = 0; j<Char.Length; j++)
+            for (int j = 0; j < Char.Length; j++)
             {
                 if (id[i] == Char[j].id)
                 {
+                    //다중공격옵션
                     multishot[i] = Char[j].multishot;
+                    numberOfProjectiles[i] = Char[j].numberOfProjectiles;
+                    //단일공격옵션
+                    singleshot[i] = Char[j].singleshot;
+                    numberOfSingleProjectiles[i] = Char[j].numberOfSingleProjectiles;
+                    //종족
                     type[i] = Char[j].type;
+                    //스탯
                     maxHp[i] = Char[j].maxHp;
                     nowHp[i] = Char[j].nowHp;
                     atkDamage[i] = Char[j].atkDamage;
                     criticalRate[i] = Char[j].criticalRate;
                     atkSpeed[i] = Char[j].atkSpeed;
+                    //스킬 스탯
                     skillCooldown[i] = Char[j].skillCooldown;
                     skillDamage[i] = Char[j].skillDamage;
+                    //메인스탯
                     maxExp[i] = Char[j].maxExp;
                     nowExp[i] = Char[j].nowExp;
                     shield[i] = Char[j].shield;
