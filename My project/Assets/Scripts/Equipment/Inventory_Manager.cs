2d871012ed4922d78965e12318d38b3745c08891
byungwoo0601 <rlaquddn0601@naver.com>
2023-06-06 23:49:59 +0900

    20230606
    인벤토리 메니저 버그 수정

--- My project/Assets/Scripts/Equipment/Inventory_Manager.cs
+++ My project/Assets/Scripts/Equipment/Inventory_Manager.cs
@@ -116,8 +116,8 @@ public class Inventory_Manager : MonoBehaviour
     {
         if(curSlot != null)
         {
-            selectedItem.type
-            eqiopSlots[]
+            //selectedItem.type
+            //eqiopSlots[]
         }
     }
 }
